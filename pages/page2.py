82c53368226fc9121fac77ed3010923c6de2fcd3
Thy Lan Etesse <thylanetesse@gmail.com>
2026-09-17 16:53:41 +0200

    page2

--- pages/page2.py
+++ pages/page2.py
@@ -1,10 +1,11 @@
-from dash import html, register_page
+from dash import html, register_page, dcc, callback
+from dash.dependencies import Input, Output
 
 register_page(__name__, name='Page 2')
 
 opt = ["oui", "non"]
 
-app.layout = html.Div([
+layout = html.Div([
 
     html.H1("Aimez-vous Brawl Star:"),
 
@@ -15,8 +16,8 @@ app.layout = html.Div([
 ])
 
 @callback(
-    Output('out','children'),
-    Input('radio','value')
+    Output('out', 'children'),
+    Input('radio', 'value')
 )
 def update(value):
     if value is None:
@@ -24,8 +25,5 @@ def update(value):
     elif value == "oui":
         txt = "Génial, jouons ensemble !"
     else:
-        txt = "Dommage, réflechissez encore"
+        txt = "Dommage, réflechis encore"
     return txt
-
-if __name__ == '__main__':
-    app.run(debug=True)
